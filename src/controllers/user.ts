383813eb9799aec3a22a474ca769c13be083ff3f
samsquareed <samsquare.professional@gmail.com>
2022-10-25 13:00:57 +0530

    user update api

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { request } from "http";
-import { getUserFromRepository, saveUserToRepository } from "../repository/userRepository";
+import { getUserFromRepository, saveUserToRepository, updateUserRepository } from "../repository/userRepository";
 
 
 export const getUser = async (req : Request, res : Response) => {
@@ -26,4 +26,16 @@ export const createUser =async (req: Request, res : Response) => {
             message : "failed to create new user"
         })
     }   
+}
+
+export const updateUser = async (req : Request, res : Response) => {
+    try {
+        const {first_name, last_name, email} = req.body;
+        const updateResponse = await updateUserRepository(parseInt(req.params.id), first_name,last_name,email);
+        updateResponse && res.status(200).json(updateResponse)
+    } catch (error) {
+        res.status(400).json({
+            message : "failed to create new user"
+        })
+    } 
 }
